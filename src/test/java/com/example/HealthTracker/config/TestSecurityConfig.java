90e5d78efb795e2b80b871c693b044c2ec1bd4d5
theboss17121999 <46495612+theboss17121999@users.noreply.github.com>
2026-06-19 22:19:30 +0530

    Added tag

--- src/test/java/com/example/HealthTracker/config/TestSecurityConfig.java
+++ src/test/java/com/example/HealthTracker/config/TestSecurityConfig.java
@@ -1,20 +1,56 @@
 package com.example.HealthTracker.config;
 
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @TestConfiguration
 public class TestSecurityConfig {
 
-    @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity http)
-            throws Exception {
+    @Mock
+    private AuthenticationProvider authenticationProvider;
 
-        return http
-                .csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
-                .build();
+    @Mock
+    private AuthenticationManager authenticationManager;
+
+    @Mock
+    private CorsConfigurationSource corsConfigurationSource;
+
+    @InjectMocks
+    public SecurityConfig  securityConfig;
+
+    @Test
+    void authProviderShouldBeCreated() {
+        AuthenticationProvider provider = securityConfig.authProvider();
+
+        assertNotNull(provider);
+        assertTrue(provider instanceof DaoAuthenticationProvider);
+    }
+    @Test
+    void authProvider() {
+    }
+
+    @Test
+    void testSecurityFilterChain() {
+    }
+
+    @Test
+    void corsConfigurationSource() {
+    }
+
+    @Test
+    void authenticationManager() {
     }
 }
